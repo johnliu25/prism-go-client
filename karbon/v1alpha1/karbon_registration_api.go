ae2433ba45062c8344412d58925cfc27a4b2d23e
zihui.liu <zihui.liu@nutanix.com>
2023-01-05 02:19:25 -0800

    Update

--- karbon/v1alpha1/karbon_registration_api.go
+++ karbon/v1alpha1/karbon_registration_api.go
@@ -1,35 +1,20 @@
-package karbon
+package v1alpha1
 
 import (
-	"fmt"
-	"strings"
+	"context"
+	"net/http"
 
-	prismgoclient "github.com/nutanix-cloud-native/prism-go-client"
-	"github.com/nutanix-cloud-native/prism-go-client/internal"
+	"github.com/nutanix-cloud-native/prism-go-client/karbon"
 )
 
-const (
-	absolutePath = "karbon"
-	userAgent    = "nutanix"
-	clientName   = "karbon"
-)
-
-// Client manages the V3 API
-type Client struct {
-	httpClient      *internal.Client
-	Cluster         ClusterService
-	PrivateRegistry PrivateRegistryService
-	Meta            MetaService
-}
-
-/*type K8sRegistrationService interface {
+type K8sRegistrationService interface {
 	CreateK8sRegistration(ctx context.Context, createRequest *K8sCreateClusterRegistrationRequest) (*K8sCreateClusterRegistrationResponse, error)
 	DeleteK8sRegistration(ctx context.Context) (*K8sClusterRegistrationDeleteResponse, error)
 	GetK8sRegistration(ctx context.Context) (*K8sClusterRegistration, error)
 	GetK8sRegistrationList(ctx context.Context) (*K8sClusterRegistrationList, error)
-}*/
+}
 
-/*func (op Client) CreateK8sRegistration(ctx context.Context, createRequest *K8sCreateClusterRegistrationRequest) (*K8sCreateClusterRegistrationResponse, error) {
+func (op karbon.Client) CreateK8sRegistration(ctx context.Context, createRequest *K8sCreateClusterRegistrationRequest) (*K8sCreateClusterRegistrationResponse, error) {
 	path := "/v1-alpha.1/k8s/cluster-registrations/"
 	req, err := op.httpClient.NewRequest(http.MethodPost, path, createRequest)
 	if err != nil {
@@ -42,7 +27,7 @@ type Client struct {
 	return karbonClusterActionResponse, nil
 }
 
-func (op Client) DeleteK8sRegistration(ctx context.Context) (*K8sClusterRegistrationDeleteResponse, error) {
+func (op karbon.Client) DeleteK8sRegistration(ctx context.Context) (*K8sClusterRegistrationDeleteResponse, error) {
 	path := "/v1-alpha.1/k8s/cluster-registrations/eae7fe7e-34e8-4978-bb9a-e49157e858d6"
 	req, err := op.httpClient.NewRequest(http.MethodDelete, path, nil)
 	if err != nil {
@@ -55,7 +40,7 @@ func (op Client) DeleteK8sRegistration(ctx context.Context) (*K8sClusterRegistra
 	return karbonClusterActionResponse, nil
 }
 
-func (op Client) GetK8sRegistration(ctx context.Context) (*K8sClusterRegistration, error) {
+func (op karbon.Client) GetK8sRegistration(ctx context.Context) (*K8sClusterRegistration, error) {
 	path := "/v1-alpha.1/k8s/cluster-registrations/eae7fe7e-34e8-4978-bb9a-e49157e858d6"
 	req, err := op.httpClient.NewRequest(http.MethodGet, path, nil)
 	if err != nil {
@@ -68,7 +53,7 @@ func (op Client) GetK8sRegistration(ctx context.Context) (*K8sClusterRegistratio
 	return karbonClusterActionResponse, nil
 }
 
-func (op Client) GetK8sRegistrationList(ctx context.Context) (*K8sClusterRegistrationList, error) {
+func (op karbon.Client) GetK8sRegistrationList(ctx context.Context) (*K8sClusterRegistrationList, error) {
 	path := "/v1-alpha.1/k8s/cluster-registrations/eae7fe7e-34e8-4978-bb9a-e49157e858d6"
 	req, err := op.httpClient.NewRequest(http.MethodGet, path, nil)
 	if err != nil {
@@ -79,35 +64,4 @@ func (op Client) GetK8sRegistrationList(ctx context.Context) (*K8sClusterRegistr
 		return nil, err
 	}
 	return karbonClusterActionResponse, nil
-}*/
-
-// NewKarbonAPIClient return a internal to operate Karbon resources
-func NewKarbonAPIClient(credentials prismgoclient.Credentials) (*Client, error) {
-	if credentials.URL == "" || credentials.Username == "" || credentials.Password == "" {
-		return nil, fmt.Errorf("karbon Client is missing: %s %s",
-			"Please provide required details - %s in provider configuration",
-			strings.Join(credentials.RequiredFields[clientName], ", "))
-	}
-	c, err := internal.NewClient(
-		internal.WithCredentials(&credentials),
-		internal.WithUserAgent(userAgent),
-		internal.WithAbsolutePath(absolutePath))
-	if err != nil {
-		return nil, err
-	}
-
-	f := &Client{
-		httpClient: c,
-		Cluster: ClusterOperations{
-			httpClient: c,
-		},
-		PrivateRegistry: PrivateRegistryOperations{
-			httpClient: c,
-		},
-		Meta: MetaOperations{
-			httpClient: c,
-		},
-	}
-
-	return f, nil
 }
