cd8f8ee5a65a915bc3f3970009e7397fb376b40e
zihui.liu <zihui.liu@nutanix.com>
2023-01-06 12:14:14 -0800

    Update

--- karbon/v1alpha1/karbon_registration_api.go
+++ karbon/v1alpha1/karbon_registration_api.go
@@ -9,10 +9,12 @@ import (
 
 var _ K8sRegistrationService = &V1Alpha1{}
 
+// V1Alpha1 defines v1alpha1 APIs
 type V1Alpha1 struct {
 	httpClient *internal.Client
 }
 
+// K8sRegistrationService defines the create, delete, get and get the list of the k8s registration
 type K8sRegistrationService interface {
 	CreateK8sRegistration(ctx context.Context, createRequest *K8sCreateClusterRegistrationRequest) (*K8sCreateClusterRegistrationResponse, error)
 	DeleteK8sRegistration(ctx context.Context, UUID string) (*K8sClusterRegistrationDeleteResponse, error)
@@ -20,6 +22,7 @@ type K8sRegistrationService interface {
 	GetK8sRegistrationList(ctx context.Context) (*K8sClusterRegistrationList, error)
 }
 
+// CreateK8sRegistration creates the k8s registration
 func (op V1Alpha1) CreateK8sRegistration(ctx context.Context, createRequest *K8sCreateClusterRegistrationRequest) (*K8sCreateClusterRegistrationResponse, error) {
 	path := "/v1-alpha.1/k8s/cluster-registrations/"
 	req, err := op.httpClient.NewRequest(http.MethodPost, path, createRequest)
@@ -33,6 +36,7 @@ func (op V1Alpha1) CreateK8sRegistration(ctx context.Context, createRequest *K8s
 	return karbonClusterActionResponse, nil
 }
 
+// DeleteK8sRegistration deletes the k8s registration with UUID
 func (op V1Alpha1) DeleteK8sRegistration(ctx context.Context, UUID string) (*K8sClusterRegistrationDeleteResponse, error) {
 	path := "/v1-alpha.1/k8s/cluster-registrations/" + UUID
 	req, err := op.httpClient.NewRequest(http.MethodDelete, path, nil)
@@ -46,6 +50,7 @@ func (op V1Alpha1) DeleteK8sRegistration(ctx context.Context, UUID string) (*K8s
 	return karbonClusterActionResponse, nil
 }
 
+// GetK8sRegistration gets the k8s registration with UUID
 func (op V1Alpha1) GetK8sRegistration(ctx context.Context, UUID string) (*K8sClusterRegistration, error) {
 	path := "/v1-alpha.1/k8s/cluster-registrations/" + UUID
 	req, err := op.httpClient.NewRequest(http.MethodGet, path, nil)
@@ -59,6 +64,7 @@ func (op V1Alpha1) GetK8sRegistration(ctx context.Context, UUID string) (*K8sClu
 	return karbonClusterActionResponse, nil
 }
 
+// GetK8sRegistrationList gets the k8s registration list
 func (op V1Alpha1) GetK8sRegistrationList(ctx context.Context) (*K8sClusterRegistrationList, error) {
 	path := "/v1-alpha.1/k8s/cluster-registrations/"
 	req, err := op.httpClient.NewRequest(http.MethodGet, path, nil)
